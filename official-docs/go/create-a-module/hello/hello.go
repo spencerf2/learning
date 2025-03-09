f8478595fe8b624a2a2de6eb6481e6c8a8affa59
Spencer Franklin <85589251+spencerf2@users.noreply.github.com>
2025-03-09 08:45:52 -0700

    Complete Add a test from Create a Go module
    - Next up:
      - Compile and install the application

--- official-docs/go/create-a-module/hello/hello.go
+++ official-docs/go/create-a-module/hello/hello.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"log"
+    "fmt"
+    "log"
 
-	"example.com/greetings"
+    "example.com/greetings"
 )
 
 func main() {
-	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flag to disable printing
-	// the time, source file, and line number.
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("")
-	// If an error was returned, print it to the console and
-	// exit the program.
-	if err != nil {
-		log.Fatal(err)
-	}
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
 
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }

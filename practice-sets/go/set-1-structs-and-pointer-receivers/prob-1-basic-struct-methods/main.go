8651cbaa5cb70f87fae7af3dd3469dd86b952550
Spencer Franklin <85589251+spencerf2@users.noreply.github.com>
2025-06-30 09:24:35 -0400

    Update problem 1 now that I know what I was missing
    - Doing problem 2 showed me what I was missing
      about problem 1!

--- practice-sets/go/set-1-structs-and-pointer-receivers/prob-1-basic-struct-methods/main.go
+++ practice-sets/go/set-1-structs-and-pointer-receivers/prob-1-basic-struct-methods/main.go
@@ -10,7 +10,7 @@ func (c Counter) GetValue() int {
 	return c.value
 }
 
-func (c Counter) SetValue(newValue int) {
+func (c *Counter) SetValue(newValue int) {
 	c.value = newValue
 }
 
@@ -35,8 +35,20 @@ func main() {
 //     is this where pointer reference vs value or copy comes into play? I think so.
 //     The function would have a copy of the struct rather than a pointer to the original.
 //     Need to confirm this though when I do problem 2.
+//     UPDATE:
+//     - Having done problem 2, I now know that using the pointer allows me to update
+//       the value the variable points to rather than just consuming the value. Using
+//       a method enables us to directly work with the variables/memory references
+//       rather than copies of the values or requiring passing the struct and/or a
+//       pointer to it around.
 // 
 // 2. Why do we use methods instead of just functions?
 //   - Because it allows us to perform related actions on the struct, rather than having
 //     to pass the struct to then act upon it, which would require passing a pointer I
 //     believe.
+//     UPDATE:
+//     - Yes, having done problem 2, I'm sure we would have to pass a pointer to the
+//       struct if we wanted to perform actions on it, rather than a copy of it. This
+//       means methods (like in other languages) enable us to group relevant actions
+//       together on the object being acted upon. They save time, reduce code complexity,
+//       and make it more maintainable through organization!

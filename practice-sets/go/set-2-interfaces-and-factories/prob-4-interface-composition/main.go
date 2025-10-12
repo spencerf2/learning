5072920879b52fe2c5df3776ac0e47e7b8208c07
Spencer Franklin <85589251+spencerf2@users.noreply.github.com>
2025-10-12 14:58:48 -0400

    chore: placeholder commit marks where I stopped 3 monhths ago
    - Returning to this after 3 months away! A little
      confusing to read these notes, but glad for them
      and ready to pick this back up and finish it!

--- practice-sets/go/set-2-interfaces-and-factories/prob-4-interface-composition/main.go
+++ practice-sets/go/set-2-interfaces-and-factories/prob-4-interface-composition/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+)
 
 type Reader interface {
 	Read(id string) ([]byte, error)
@@ -29,11 +32,11 @@ type Database struct {
 //               the way I have is the same as `ReadWriter ReadWriter`?
 
 type Cache struct {
-	read Reader
+	Reader
 }
 
 type Logger struct {
-	write Writer
+	Writer
 }
 
 // Note to self: If I do:
@@ -51,10 +54,10 @@ type Logger struct {
 //               I believe this means I must implment the Reader interface
 //               so that `read` actually points to something.
 
-func (c *Cache) read(userID string) ([]byte, error) {
-	data := []byte(fmt.Sprintf("read_in_%s", userID))
-	return data, nil
-}
+// func (c *Cache) Read(userID string) ([]byte, error) {
+// 	data := []byte(fmt.Appendf([]byte("starting_byte"), "read_in_%s", userID))
+// 	return data, nil
+// }
 
 // Note to self: If I implement a `read()` method on Cache, then there's
 //               an error message saying:
@@ -63,8 +66,19 @@ func (c *Cache) read(userID string) ([]byte, error) {
 //               main.go(32, 2): other declaration of read
 //
 //               Thinking it'll go away if I make Cache struct only contain `Reader`.
+//               It goes away if I capitalize the `R` on the `read()` method.
+//               And also if I delete `read` from Cache.
+//               I believe this is because Reader is an interface, so Cache struct
+//               doesn't need a field to represent it, although it can have one.
+//               Cache can simply embed its methods. What does that make Cache then?
+//               A struct that embeds the Reader interface. Perhaps that makes sense
+//               because Cache may need the ability to read in data.
 
-func LoadUserProfile(rw ReadWriter, userID string, newData []byte) error {
+func LoadUserProfile(r Reader, userID string) ([]byte, error) {
+	return r.Read(fmt.Sprintf("profile_%s", userID))
+}
+
+func UpdateUserProfile(rw ReadWriter, userID string, newData []byte) error {
 	// Read current data
 	_, err := rw.Read(fmt.Sprintf("profile_%s", userID))
 	if err != nil {
@@ -75,7 +89,24 @@ func LoadUserProfile(rw ReadWriter, userID string, newData []byte) error {
 	return rw.Write(fmt.Sprintf("profile_%s", userID), newData)
 }
 
+
+
 func main() {
 	db := Database{}
-	LoadUserProfile(db, "spencer_ID", []byte("cusco_data"))
+	userID := "spencer_ID"
+
+	data, err := LoadUserProfile(db, userID)
+	if err != nil {
+		slog.Error("could not load user using database")
+	}
+	slog.Info("loaded user profile from db", "data", data)
+
+	err = UpdateUserProfile(db, userID, fmt.Append(data, "added_data"))
+	if err != nil {
+		slog.Error("could not update user using database")
+	}
+
+	// c := Cache{}
+	// cacheData, err := LoadUserProfile(c, userID)
 }
+// https://claude.ai/chat/8c2fef5d-51a4-4f97-8547-c3bb60c318b1
